2c96e56a4e4312c13b3a18e827b9340fbc14abe8
Luiza Aguiar <aguiar-luiza@hotmail.com>
2019-03-29 22:47:58 -0300

    Atualizacao01

---
+++ Assets/_Source/Scripts/MainMenuController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour { 
+
+    public GameObject options;
+    public GameObject mainmenu;
+
+    // Start is called before the first frame update
+    void Start() {
+        options.SetActive(false);
+        mainmenu.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Play () {
+        SceneManager.LoadScene("Fase01");
+    }
+
+    public void Options () {
+        options.SetActive(true);
+        mainmenu.SetActive(false);
+    }
+
+    public void Quit () {
+        
+    }
+
+}
