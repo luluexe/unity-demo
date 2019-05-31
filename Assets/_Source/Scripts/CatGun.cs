085092f4fd01a4643d48c1898536278e66ecccfc
Luiza Aguiar <aguiar-luiza@hotmail.com>
2019-05-31 00:20:17 -0300

    02

--- Assets/_Source/Scripts/CatGun.cs
+++ Assets/_Source/Scripts/CatGun.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CatGun : MonoBehaviour
-{ 
+{
+
     public float damage = 10f;
     public float range = 100f;
 
     public Camera fpsCam;
-    
+   
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown ("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
@@ -21,9 +22,15 @@ public class CatGun : MonoBehaviour
     void Shoot ()
     {
         RaycastHit hit;
-        if (Physics.Raycast (fpsCam.transform.position , fpsCam.transform.forward, out hit, range))
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) 
         {
             Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
         }
     }
 }
